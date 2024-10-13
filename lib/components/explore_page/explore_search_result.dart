b77095179d05ede2041bb6e8a85317f5f2457df8
Javkhlant Altankhuyag <enrosek95@gmail.com>
2024-10-13 21:13:11 +0800

    Finish explore search results

--- lib/components/explore_page/explore_search_result.dart
+++ lib/components/explore_page/explore_search_result.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/components/explore_page/explore_page_categories.dart';
+import 'package:news_app/components/explore_page/explore_page_news_item.dart';
+import 'package:news_app/utils/thememode_color.dart';
 
 class ExploreSearchResult extends StatelessWidget {
   const ExploreSearchResult({super.key});
 
+  final List<int> data = const [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundPrimary(
+          isLightMode(context),
+        ),
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Search Results",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        centerTitle: true,
+        leadingWidth: 80,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(14),
+            child: Icon(
+              Icons.arrow_back,
+              size: 24,
+              color: textPrimary(isLightMode(context)),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 16,
+        ),
+        child: Column(
+          children: [
+            const ExplorePageCategories(),
+            const SizedBox(height: 24),
+            Expanded(
+              child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 32)
+                    .copyWith(bottom: 32),
+                itemBuilder: (context, index) => ExplorePageNewsItem(
+                  key: ValueKey(data[index]),
+                ),
+                separatorBuilder: (context, index) => SizedBox(
+                  height: 24,
+                  key: ValueKey(index),
+                ),
+                itemCount: data.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
